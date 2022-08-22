fbf4a1a59c2550033db4d7a7942a78443fe17af2
amirdrljevic <amirdrljevic@gmail.com>
2022-08-22 21:16:09 +0200

    Edit views edited (#9)

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,4 @@ class Company < ApplicationRecord
   validates :company_name, presence: true, length: { minimum: 3 }
   validates :country, presence: true, length: { minimum: 3 }
   validates :city, presence: true, length: { minimum: 3 }
-
-
 end
