41a56508ff751765a03ec7166cfbe48bd945d674
Amir Drljevic <amirdrljevic@gmail.com>
2022-08-26 20:36:37 +0200

    faker and seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Generate some rows for Department model
+Department.destroy_all
+10.times do |n|
+  field = Faker::Job.field
+  Department.create!(
+    department_name: field
+  )
+end
+
+# Generate some rows for Role model
+Role.destroy_all
+10.times do |n|
+  position = Faker::Job.position
+  Role.create!(
+    role_name: position
+  )
+end
+
+# Generate some rows for Company model
+Company.destroy_all
+10.times do |n|
+  name = Faker::Company.name
+  addr = Faker::Address.street_address
+  code = Faker::Address.zip_code.to_i
+  city = Faker::Address.city
+  ctry = Faker::Address.country
+  Company.create!(
+    company_name:   name,
+    street_address: addr,
+    postal_code:    code,
+    city:           city,
+    country:        ctry
+  )
+end
