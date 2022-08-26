65e936b6c4baf4e84f3b48d18ad3bb593e3ae8de
amirdrljevic <amirdrljevic@gmail.com>
2022-08-26 19:54:49 +0200

    debugger fixed (#11)

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,9 @@ class Company < ApplicationRecord
   validates :company_name, presence: true, length: { minimum: 3 }
   validates :country, presence: true, length: { minimum: 3 }
   validates :city, presence: true, length: { minimum: 3 }
+
+
+  validates :postal_code, numericality: true
+  #validates :postal_code, numericality: { only_integer: true }
+
 end
