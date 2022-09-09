cb118ced0bbc2039dc189ebbd5c161f8b83824bf
Amir Drljevic <amirdrljevic@gmail.com>
2022-09-09 20:24:18 +0200

    fixed error msgs on devise sign up

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -308,4 +308,30 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+  class TurboFailureApp < Devise::FailureApp
+    def respond
+      if request_format == :turbo_stream
+        redirect
+      else
+        super
+      end
+    end
+  
+    def skip_format?
+      %w(html turbo_stream */*).include? request_format.to_s
+    end
+  end
+  
+   Devise.setup do |config|
+    ...
+    # Configure the parent class to the custom controller.
+    config.parent_controller = 'TurboDeviseUserController'
+    config.navigational_formats = ['*/*', :html, :turbo_stream]
+  
+  
+    # Warden configuration
+    config.warden do |manager|
+      manager.failure_app = TurboFailureApp
+    end
+  end
 end
