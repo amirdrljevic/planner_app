f14bb60d268e2f526150ad7717eb23e4b7dc438f
Amir Drljevic <amirdrljevic@gmail.com>
2022-09-19 22:39:32 +0200

    Protect unauthorized access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,17 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, 
-                                                       :last_name, 
-                                                       :role_id, 
-                                                       :department_id, 
-                                                       :company_id, 
-                                                       :date_of_birth, 
-                                                       :position, 
-                                                       :office_phone, 
-                                                       :mobile_phone, 
-                                                       :private_phone])
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, 
+                                                        :last_name, 
+                                                        :role_id, 
+                                                        :department_id, 
+                                                        :company_id, 
+                                                        :date_of_birth, 
+                                                        :position, 
+                                                        :office_phone, 
+                                                        :mobile_phone, 
+                                                        :private_phone])
+    end
+    
 end
--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,4 +1,5 @@
 class RolesController < ApplicationController
+  before_action :logged_in_user  
   before_action :set_role, only: %i[ show edit update destroy ]
 
   # GET /roles or /roles.json
@@ -67,4 +68,14 @@ class RolesController < ApplicationController
     def role_params
       params.require(:role).permit(:role_name)
     end
+
+    # Check if user is logged in
+    def logged_in_user
+      unless user_signed_in?
+        respond_to do |format|
+          format.html { redirect_to new_user_session_path, notice: "Restricted access. Please sign in." }
+          format.json { head :no_content }      
+        end
+      end
+    end    
 end
