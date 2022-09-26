f04b89e28a3fb45761a332614317906c79e62538
Amir Drljevic <amirdrljevic@gmail.com>
2022-09-26 21:22:59 +0200

    Separate user and devise controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   #resources :users, only: [:index, :show] #leave it for now, but it will probably go
-  resources :users, :controller => "users" 
+  resources :users_admin, :controller => "users" 
   #get 'home/index'
   resources :companies
   resources :roles
