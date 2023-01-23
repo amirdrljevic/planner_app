aa0c6e46b1a55c8e23f95941e637ef308054bb99
amirdrljevic <amirdrljevic@gmail.com>
2023-01-23 21:11:12 +0100

    Fix error msg while deleting foreign key (#57)

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,7 +2,7 @@ class Department < ApplicationRecord
   validates :department_name, presence: true, length: { minimum: 3 }
   scope :ordered, -> { order(id: :desc) }
 
-  has_many :users, dependent: :destroy
+  has_many :users
   #TODO: Check and implement what happens if a user tries to delete a referenced department.
   #TODO: if user deletes department, it shouldn't be available if that department_id is being used in users table
   #a message should pop up
